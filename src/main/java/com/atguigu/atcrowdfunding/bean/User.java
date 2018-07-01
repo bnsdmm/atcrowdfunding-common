aa4d77d68545195296e764b77c4ea80fbeee565f
wangzhen <506826727@qq.com>
2018-07-01 08:38:49 +0800

    登录跳转+字符乱码

--- src/main/java/com/atguigu/atcrowdfunding/bean/User.java
+++ src/main/java/com/atguigu/atcrowdfunding/bean/User.java
@@ -2,20 +2,41 @@ package com.atguigu.atcrowdfunding.bean;
 
 public class User {
 
-	
 	private Integer id;
 	private String username;
+	private String loginacct;
+	private String userpswd;
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	
+
+	public String getLoginacct() {
+		return loginacct;
+	}
+
+	public void setLoginacct(String loginacct) {
+		this.loginacct = loginacct;
+	}
+
+	public String getUserpswd() {
+		return userpswd;
+	}
+
+	public void setUserpswd(String userpswd) {
+		this.userpswd = userpswd;
+	}
+
 }
